4c25ef3effa9dcd158c6a330e797445973081650
Subham Biswas <subhambiswas129@gmail.com>
2025-05-28 19:12:11 +0530

    minor changes

--- lib/data/repository/auth_repository.dart
+++ lib/data/repository/auth_repository.dart
@@ -1,8 +1,11 @@
+import 'package:customer_app/data/model/user_cache_model.dart';
+import 'package:customer_app/utils/cache_manager.dart';
 import 'package:customer_app/utils/constants.dart';
 import 'package:customer_app/utils/storage_manager.dart';
 
 class AuthRepository {
   final StorageManager _storageManager = StorageManager();
+  final CacheManager _cacheManager = CacheManager();
   AuthRepository();
 
   Future<bool?> authenticate({required String username, required String password}) async {
@@ -18,6 +21,6 @@ class AuthRepository {
   }
 
   Future<bool?> unAuthenticate() async{
-    return await _storageManager.clear();
+    return await _storageManager.clear()&&await _cacheManager.clearBox<UserCacheModel>(HiveBoxKeys.userModelBoxKey);
   }
 }
